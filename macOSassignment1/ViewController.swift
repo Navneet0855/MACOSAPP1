5376b3bf861142e8cb294ae9a72f03faaacd4a0f
Navneet Brar Group2 <navneetkaur17071995@gmail.com>
2020-07-26 11:22:59 -0700

    created macOSassignment1

---
+++ macOSassignment1/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  macOSassignment1
+//
+//  Created by Navneet Brar on 2020-07-25.
+//  Copyright © 2020 Navneet Brar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+override    func viewDidAppear(_ animated: Bool)
+    {
+        self.performSegue(withIdentifier: "LoginView", sender: self)
+    }
+    
+}
+
